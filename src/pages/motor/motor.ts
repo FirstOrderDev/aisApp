179a4acad7eeb89df3d6e33786c794c389cc40c7
Harrison Croaker <harrisoncroaker@Harrisons-MacBook-Pro.local>
2017-12-12 12:26:56 +1000

    Grid

--- src/pages/motor/motor.ts
+++ src/pages/motor/motor.ts
@@ -147,7 +147,8 @@ export class MotorPage {
      // imageData is either a base64 encoded string or a file URI
      // If it's base64:
      let base64Image = 'data:image/jpeg;base64,' + imageData;
-     this.images.push(base64Image);
+     this.images.push({
+       pic: base64Image});
     }, (err) => {
      // Handle error
     });
