3001a367fd0e8707beac3d2be6e8c319e41256d2
Mrpyro22 <gallade22@gmail.com>
2017-12-12 14:27:00 +1000

    email

--- src/pages/motor/motor.ts
+++ src/pages/motor/motor.ts
@@ -239,7 +239,7 @@ export class MotorPage {
 
       ],
       subject: 'Cordova Icons',
-      body: 'How are you? Nice greetings from Leipzig' + this.policyInput,
+      body: 'Policy Number: ' + this.policyInput + '<br />' +  'Name: ' + this.nameInput,
       isHtml: true
     };
 
