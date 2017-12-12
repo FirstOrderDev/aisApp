db0d912346a86d132f208da76786e43d791a1266
Mrpyro22 <gallade22@gmail.com>
2017-12-12 11:48:16 +1000

    camera

--- src/pages/motor/motor.ts
+++ src/pages/motor/motor.ts
@@ -8,6 +8,7 @@ import { AlertController } from 'ionic-angular';
 import { LoadingController } from 'ionic-angular';
 import { ImagePicker } from '@ionic-native/image-picker';
 import { Base64 } from '@ionic-native/base64';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
 /**
@@ -43,7 +44,7 @@ export class MotorPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private geolocation: Geolocation,
     private nativeGeocoder: NativeGeocoder, private alertCtrl: AlertController, private imagePicker: ImagePicker,
-    private base64: Base64, public loadingCtrl: LoadingController) {
+    private base64: Base64, public loadingCtrl: LoadingController, private camera: Camera) {
     this.currentCard = 0;
     this.address = "Enter an address"
 
@@ -129,6 +130,27 @@ export class MotorPage {
     ]
   });
   alert.present();
+  }
+
+  //step 6 (card 5)
+
+  openCamera(){
+
+    const options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.images.push(base64Image);
+    }, (err) => {
+     // Handle error
+    });
   }
 
   select(){
